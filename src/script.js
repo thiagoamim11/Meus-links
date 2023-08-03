de91a4c37a3d836f54734c27a7e815efbd0db351
Thiago Amim <thiagoamim11@gmail.com>
2023-08-02 23:59:56 -0300

    Projeto finalizado com sucecsso

---
+++ src/script.js
@@ -0,0 +1,24 @@
+
+function toggleMode() {
+  const html = document.documentElement
+/*
+  if(html.classList.contains('light')) {
+    html.classList.remove('light')
+
+  } 
+  else {
+    html.classList.add('light')
+  }
+*/
+
+ html.classList.toggle('light');
+
+ /*
+ const img = document.querySelector("#profile img")
+ 
+ if(html.classList.contains('light')) {
+  img.setAttribute("src", "../assets/assets/eu2.jpg")
+ } else {
+    img.setAttribute("src", "../assets/assets/eu.jpg")
+ }   */
+}
